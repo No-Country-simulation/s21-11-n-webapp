8697875a22e7915a232f158978186e26732251be
imsotodev <sotopaguayj@gmail.com>
2025-03-04 20:21:09 -0500

    revert(FRONT): :ambulance: remove app backend folder

--- app/src/main/java/com/bakesoft/auth/application/dto/RefreshTokenRequest.java
+++
@@ -1,6 +0,0 @@
-package com.bakesoft.auth.application.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record RefreshTokenRequest(@NotBlank(message = "Refresh token cannot be empty") String refreshToken) {
-}
