87f83ae45ebe95cd54400ba7d23ea438a8909570
imsotodev <sotopaguayj@gmail.com>
2025-03-04 20:17:27 -0500

    fix(FRONT): :bug: bug

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react(), tailwindcss()],
   test: {
     globals: true,
